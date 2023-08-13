d7bc4bc0bb3c602a07898c87711936cc8cbdbc6d
MahmoudToubar <110629991+MahmoudToubar@users.noreply.github.com>
2023-08-13 22:00:24 +0300

    Update Employee.cs

--- Employee Management/Models/Employee.cs
+++ Employee Management/Models/Employee.cs
@@ -20,6 +20,7 @@ namespace Employee_Management.Models
 
 
         [Required]
+        [StringLength(7)]
         [RegularExpression(@"^[0-9]+$", ErrorMessage = "ID must be a number")]
         public string ID { get; set; }
 
